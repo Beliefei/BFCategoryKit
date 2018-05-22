9287b85d080f52f3a8a52caddf28027aa8140cce
Beliefei <duanlongfed@126.com>
2018-05-22 17:24:33 +0800

    删除有依赖的文件

---
+++ Example/Pods/Target Support Files/BFCategoryKit/BFCategoryKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BFCategoryKit : NSObject
+@end
+@implementation PodsDummy_BFCategoryKit
+@end
---
+++ Example/Pods/Target Support Files/BFCategoryKit/BFCategoryKit-umbrella.h
@@ -0,0 +1,41 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "UIAlertView+UserInfo.h"
+#import "UIButton+KB_ImageTitleSpacing.h"
+#import "UIColor+expanded.h"
+#import "UIDevice+Hardware.h"
+#import "UIFont+Level.h"
+#import "UIImageView+Create.h"
+#import "UILabel+KB_Factory.h"
+#import "UIImage+Additions.h"
+#import "UIImage+CircleImage.h"
+#import "UIImage+Color.h"
+#import "UIImage+ComposeImage.h"
+#import "UIImage+EMUtils.h"
+#import "UIImage+GrayImage.h"
+#import "UIImage+launchImage.h"
+#import "UIImage+MultipliedAlpha.h"
+#import "UIResponder+TRCEMRouter.h"
+#import "UITableView+ShowEmptyTip.h"
+#import "UITextView+Placeholder.h"
+#import "UIView+Borders.h"
+#import "UIView+Corner.h"
+#import "UIView+Genie.h"
+#import "UIView+SafeArea.h"
+#import "UIView+TargetAction.h"
+#import "UIViewController+log.h"
+#import "UIViewController+SafeArea.h"
+
+FOUNDATION_EXPORT double BFCategoryKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char BFCategoryKitVersionString[];
+
