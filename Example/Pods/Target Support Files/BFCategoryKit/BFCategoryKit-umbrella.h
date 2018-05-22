95105bc291b115e9af77e3029d1fa02972bac75c
Beliefei <duanlongfed@126.com>
2018-05-22 17:53:47 +0800

    xxxx

--- Example/Pods/Target Support Files/BFCategoryKit/BFCategoryKit-umbrella.h
+++ Example/Pods/Target Support Files/BFCategoryKit/BFCategoryKit-umbrella.h
@@ -10,14 +10,6 @@
 #endif
 #endif
 
-#import "UIAlertView+UserInfo.h"
-#import "UIButton+KB_ImageTitleSpacing.h"
-#import "UIColor+expanded.h"
-#import "UIDevice+Hardware.h"
-#import "UIFont+Level.h"
-#import "UIImageView+Create.h"
-#import "UILabel+KB_Factory.h"
-#import "UIImage+Additions.h"
 #import "UIImage+CircleImage.h"
 #import "UIImage+Color.h"
 #import "UIImage+ComposeImage.h"
@@ -25,16 +17,6 @@
 #import "UIImage+GrayImage.h"
 #import "UIImage+launchImage.h"
 #import "UIImage+MultipliedAlpha.h"
-#import "UIResponder+TRCEMRouter.h"
-#import "UITableView+ShowEmptyTip.h"
-#import "UITextView+Placeholder.h"
-#import "UIView+Borders.h"
-#import "UIView+Corner.h"
-#import "UIView+Genie.h"
-#import "UIView+SafeArea.h"
-#import "UIView+TargetAction.h"
-#import "UIViewController+log.h"
-#import "UIViewController+SafeArea.h"
 
 FOUNDATION_EXPORT double BFCategoryKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char BFCategoryKitVersionString[];
